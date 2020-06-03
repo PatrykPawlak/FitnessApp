0fba43dd37a0be63a994e3a7f9eba5cdbfe00996
Patryk Pawlak <patrykpawlak.pl@gmail.com>
2020-06-03 22:42:05 +0200

    [feat](about-screen): Init AboutScreen

--- lib/app/fitness_app_layout.dart
+++ lib/app/fitness_app_layout.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:FitnessApp/screens/about_screen.dart';
 
 class FitnessAppLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('FitnessApp'),
+        child: AboutScreen(),
+        // child: Text('FitnessApp'),
       ),
     );
   }

0217a8831e2b1f2703759c1d727317471f22b882
Patryk Pawlak <patrykpawlak.pl@gmail.com>
2020-07-14 15:59:25 +0200

    [feat](router): Add new routes
    - Add Splash, ExerciseDetail, TrainingDetail, TrainingPlanDetail,
      ExerciseEdit, TrainingEdit, TrainingPlanEdit routes

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,7 +1,19 @@
 class Routes {
+  //main routes
+  static const String splashScreenRoute = 'splash';
   static const String homeScreenRoute = 'home';
   static const String exercisesScreenRoute = 'exercises';
   static const String trainingsScreenRoute = 'trainings';
   static const String trainingPlansScreenRoute = 'trainingPlans';
   static const String settingsScreenRoute = 'settings';
+
+  //detail data routes
+  static const String exerciseDetailScreenRoute = 'exerciseDetail';
+  static const String trainingDetailScreenRoute = 'trainingDetail';
+  static const String trainingPlanDetailScreenRoute = 'trainingPlanDetail';
+
+  //edit data routes
+  static const String exerciseEditScreenRoute = 'exerciseEdit';
+  static const String trainingEditScreenRoute = 'trainingDetail';
+  static const String trainingPlanEditScreenRoute = 'trainingPlanEdit';
 }
